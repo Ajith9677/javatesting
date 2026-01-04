47249ba482f27e928eaf28d836ece59739109fe4
Ajith9677 <ajithksnew@gmail.com>
2026-01-04 23:15:01 +0530

    Create test.java
    test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+public class javaclass(){
+
+public static void class(){
+   System.out.printin("javaclassday");
+}
+public static void main(String [] args){
+    javaclass java = new javaclass();
+    java.class();
+ }
+}
